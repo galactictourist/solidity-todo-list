117dbf4a323f85ed6bef6c152f2b84c9fd70ab6b
galactictourist <galactictourist@gmail.com>
2019-03-22 21:06:53 -0700

    list tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,26 @@ pragma solidity ^0.5.0;
 contract TodoList {
 
     uint public taskCount = 0;
+
+//model task object
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+//put tasks in state
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Default task");
+    }
+
+//add new task
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
